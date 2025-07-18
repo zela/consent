d48b2db9f4dc82af5dd0160532ec89f83d1fa9f4
Igor Zelenin <i@zelamundo.org>
2025-07-18 23:36:22 +0200

    test: adjust the testing configs for the convenience

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   test: {
     browser: {
       enabled: true,
+      headless: true,
       provider: "playwright",
       instances: [{ browser: "chromium" }],
     },
