24201d18443531c30c17f7e41dc1f85c84c0ec0e
Igor Zelenin <i@zelamundo.org>
2025-07-18 02:28:54 +0200

    fix: provider imports in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "@/components/auth/AuthContext.tsx";
-import { ConsentProvider } from "@/components/consent/ConsentContext.tsx";
+import { AuthProvider } from "@/components/auth/AuthProvider.tsx";
+import { ConsentProvider } from "@/components/consent/ConsentProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
